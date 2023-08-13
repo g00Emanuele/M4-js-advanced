f2bb02350667537ee6514e6b1ef444b07135c853
g00Emanuele <g00emanuele@gmail.com>
2023-08-13 22:43:21 +0200

    eminem fetch

--- d2/HW2/main.js
+++ d2/HW2/main.js
@@ -1,71 +1,23 @@
-// AJAX
+window.onload = () => {
+  fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem')
+    .then((data) => data.json())
+    .then((data) => {
+      const eminemSection = document.getElementById('eminemSection')
+      eminemSection.innerHTML = data.data.map((card) =>{
 
-function makeGetAjaxCall(url, callback) {
-  
-    const xhr = new XMLHttpRequest();
-    
-    xhr.open("GET", url);
-  
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        callback(null, xhr.response)
-      } else {
-        callback('Errore durante la richiesta')
-      }
-    }
-  
-    xhr.onerror = function() {
-      callback('Errore di rete')
-    }
-  
-    xhr.send()
-  }
-
-  makeGetAjaxCall('https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem', function(err, response) {
-  if (err) {console.log(err)}
-  else {console.log(JSON.parse(response))
-  
-  const a = JSON.parse(response)
-  
-  const eminem = document.getElementById('eminem')
-  eminem.classList.remove('d-none')
-  for(i = 0; i < a.data.length; i++){
-    const listaCanzoni = document.createElement('p')
-    listaCanzoni.innerText = a.data[i].title
-    eminem.appendChild(listaCanzoni)
-  }
-  }
-  })
-
-  makeGetAjaxCall('https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica', function(err, response) {
-  if (err) {console.log(err)}
-  else {console.log(JSON.parse(response))
-  
-  const a = JSON.parse(response)
-  
-  const metallica = document.getElementById('metallica')
-  metallica.classList.remove('d-none')
-  for(i = 0; i < a.data.length; i++){
-    const listaCanzoni = document.createElement('p')
-    listaCanzoni.innerText = a.data[i].title
-    metallica.appendChild(listaCanzoni)
-  }
-  }
-  })
-
-  makeGetAjaxCall('https://striveschool-api.herokuapp.com/api/deezer/search?q=queen', function(err, response) {
-  if (err) {console.log(err)}
-  else {console.log(JSON.parse(response))
-  
-  const a = JSON.parse(response)
-  
-  const queen = document.getElementById('queen')
-  queen.classList.remove('d-none')
-  for(i = 0; i < a.data.length; i++){
-    const listaCanzoni = document.createElement('p')
-    listaCanzoni.innerText = a.data[i].title
-    queen.appendChild(listaCanzoni)
-  }
-  }
-  })
+        return `<div class="col-3">
+                <div class="card">
+                    <img src="${card.album.cover_xl}" class="card-img-top img-fluid">
+                    <div class="card-body">
+                        <h5 class="card-title">${card.title}</h5>
+                        <p class="card-text">${card.album.title}</p>
+                    </div>
+                </div>
+            </div>`
+      }).join('')
+    })
+ 
+      
+    .catch(err => console.log('Request Failed', err))
+}
 
